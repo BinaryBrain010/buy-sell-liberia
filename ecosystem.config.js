ce14fe725c44c36056843a4076a96e451a0fb24e
Muhammad Ahad <captainahad585@gmail.com>
2025-08-26 21:17:30 +0500

    changed pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,17 @@
 module.exports = {
-        "apps": [
-                {
-                        "name": "website",
-                        "script": "node_modules/next/dist/bin/next",
-                        "args": "start",
-                        "cwd": "./",
-                        "instances": "max",
-                        "exec_mode": "cluster"
-                }
-        ]
-}
+        apps: [
+          {
+            name: 'website',
+            script: 'node_modules/next/dist/bin/next',
+            args: 'start',
+            cwd: './',
+            watch: false, // Disable file watching to avoid conflicts
+            instances: 1, // Use a single instance to avoid cluster issues
+            exec_mode: 'fork', // Use fork mode instead of cluster
+            env: {
+              NODE_ENV: 'production',
+            },
+          },
+        ],
+      };
+      
